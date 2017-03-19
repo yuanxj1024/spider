4f24ebfcd6c83e531ec4fdd82dbe5b2a3f74f498
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2017-03-19 09:48:44 +0800

    add

--- index2.js
+++ index2.js
@@ -2,16 +2,21 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var sleep = require('sleep');
+var argv = process.argv;
 
 var firstUrl = 'http://www.xzqy.net/';
 
-
 var total = 1000;
+var name = '河北';
+// var target = '';
+if (argv.length > 2 && argv[2]) {
+  name = argv[2];
+}
 
 // 收集一级省市
 
 function sendRequest(url, callback) {
-  sleep.msleep(3000);
+  sleep.msleep(1600);
   console.log(url);
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -35,7 +40,6 @@ function getId(url) {
   return url.match(regId)[0] || '';
 }
 
-
 var firstData = [];
 var dataObject = {};
 
@@ -46,29 +50,29 @@ function first() {
     }
     var list = [];
     $('.navi a').each(function () {
-      var self = $(this);
-      var href = self.attr('href');
-      if (href.indexOf('htm') > 1) {
-        var id = href.match(regId)[0] || '';
-        var obj = {
-          href: href,
-          name: self.text(),
-          id: id,
-          children: {}
-        };
-        // list.push(obj);
-        // dataObject[self.text()] = obj;
-        second(obj);
-      }
+      try {
+        var self = $(this);
+        var href = self.attr('href');
+        if (href.indexOf('htm') > 1) {
+          var id = href.match(regId)[0] || '';
+          var obj = {
+            href: href,
+            name: self.text(),
+            id: id,
+            children: {}
+          };
+          // list.push(obj);
+          dataObject[self.text()] = obj;
+          // second(obj);
+        }
+      } catch (e) {}
     });
-    console.log('[success] - 收集' + list.length + '个一级省市');
-    // second(list);
+    second(dataObject[name]);
   });
 }
 
 function second(item) {
-  // var temp = [data[0]]
-  // data.map(function (item) {
+  // var temp = [data[0]] data.map(function (item) {
   console.log('[二级] - ', item.name, '处理中');
   sendRequest(firstUrl + item.href, function ($) {
     if (!$) {
@@ -76,37 +80,44 @@ function second(item) {
     }
     var list = [];
     $('.parent a').each(function () {
-      var self = $(this);
-      var href = self.attr('href');
-      var obj = {
-        href: href,
-        name: self.text(),
-        id: getId(href)
-      };
-      third(item, obj);
-      // list.push(obj);
-      // dataObject[item.name].children[self.text()] = obj;
+      try {
+
+        var self = $(this);
+        var href = self.attr('href');
+        var obj = {
+          href: href,
+          name: self.text(),
+          id: getId(href)
+        };
+        third(item, obj);
+        // list.push(obj); dataObject[item.name].children[self.text()] = obj;
+      } catch (e) {
+        writeError(e);
+      }
     });
-    // console.log('[二级] - ', item.name, '收集了', list.length, '个市级');
-    // third(item, list);
   });
   // });
 }
 
 function third(first, second) {
-  // var temp = [secondList[1]];
-  // secondList.map(function (item) {
+  // var temp = [secondList[1]]; secondList.map(function (item) {
   var item = second;
   console.log('[三级] - ', item.name, '处理中');
   sendRequest(firstUrl + item.href, function ($) {
     if (!$) {
       return;
     }
-    $('.parent').each(function () {
-      var parent = $(this);
-      var third = getObj($(parent.children('a')[0]));
-      five(first, item, third);
-    });
+    $('.parent')
+      .each(function () {
+        try {
+
+          var parent = $(this);
+          var third = getObj($(parent.children('a')[0]));
+          five(first, item, third);
+        } catch (e) {
+          writeError(e);
+        }
+      });
   });
   // });
 }
@@ -118,15 +129,24 @@ function five(first, second, third) {
     }
     var list = [];
     $('.parent').each(function () {
-      var parent = $(this);
-      var four = getObj($(parent.children('a')[0]));
-      console.log('[四级] - ', first.name, second.name, third.name, four.name, '处理中');
-      // console.log('four = ', four);
-      parent.next('td').find('a').each(function () {
-        var five = getObj($(this));
-        // console.log('[五级] -', five.name, '保存中...');
-        writeFile(toString(first, second, third, four, five));
-      });
+      try {
+        var parent = $(this);
+        var four = getObj($(parent.children('a')[0]));
+        console.log('[四级] - ', first.name, second.name, third.name, four.name, '处理中');
+        parent
+          .next('td')
+          .find('a')
+          .each(function () {
+            try {
+              var five = getObj($(this));
+              writeFile(toString(first, second, third, four, five));
+            } catch (e) {
+              writeError(e);
+            }
+          });
+      } catch (e) {
+        writeError(e);
+      }
     });
   });
 }
@@ -134,7 +154,7 @@ function five(first, second, third) {
 String.prototype.format = function (args) {
   var result = this;
   if (arguments.length > 0) {
-    if (arguments.length == 1 && typeof (args) == "object") {
+    if (arguments.length == 1 && typeof(args) == "object") {
       for (var key in args) {
         if (args[key] != undefined) {
           var reg = new RegExp("({" + key + "})", "g");
@@ -145,7 +165,7 @@ String.prototype.format = function (args) {
       for (var i = 0; i < arguments.length; i++) {
         if (arguments[i] != undefined) {
           //var reg = new RegExp("({[" + i + "]})", "g");//这个在索引大于9时会有问题，谢谢何以笙箫的指出
-          　　　　　　　　　　　　
+
           var reg = new RegExp("({)" + i + "(})", "g");
           result = result.replace(reg, arguments[i]);
         }
@@ -176,22 +196,12 @@ function getObj(self) {
   return obj;
 }
 
-var fileData = [
-  '1,3,412,3',
-  'aaaa',
-  'bbbb',
-  'ccccc',
-  'ddddd'
-]
-
-
-
 function writeFile(data) {
-  // fileData.map(function (data) {
-  fs.appendFileSync('data.csv', data + '\r\n');
-  // });
+  fs.appendFileSync(name + '.data.csv', data + '\r\n');
 }
 
-// writeFile();
+function writeError(e) {
+  fs.appendFileSync('error.txt', JSON.stringify(e) + '\r\n');
+}
 
 first();
